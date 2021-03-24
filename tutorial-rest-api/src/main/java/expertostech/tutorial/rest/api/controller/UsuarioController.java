d8327fc4adcdbb88335c94eb2675c7d7a7e94fd1
Luana Campos <luana.cam20@gmail.com>
2021-03-24 19:09:01 -0300

    Add project

---
+++ tutorial-rest-api/src/main/java/expertostech/tutorial/rest/api/controller/UsuarioController.java
@@ -0,0 +1,30 @@
+package expertostech.tutorial.rest.api.controller;
+
+
+import expertostech.tutorial.rest.api.model.UsuarioModel;
+import expertostech.tutorial.rest.api.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+
+@RestController
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository repository;
+
+    @GetMapping(path = "/api/usuario/{codigo}")
+    public ResponseEntity consultar(@PathVariable("codigo") Integer codigo) {
+        return repository.findById(codigo)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/api/usuario/salvar")
+    public UsuarioModel salvar(@RequestBody UsuarioModel usuario) {
+        return repository.save(usuario);
+    }
+
+}
